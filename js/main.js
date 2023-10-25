14ddf6fc7fce82b194d407566f96efc4a55d35d4
zzzxx729 <vvvc40.v4@gmail.com>
2023-10-25 16:13:03 +0300

    Create a project

---
+++ js/main.js
@@ -0,0 +1,126 @@
+// Variables
+var squares = document.getElementsByClassName('cell');
+var player = 'X';
+var resetBtn = document.getElementsByClassName('reset')[0];
+var winnerplayer = document.getElementsByClassName('winner')[0];
+var turn = document.getElementsByClassName('turn')[0];
+
+// Function to start the game
+for(i = 0; i < squares.length; i++) 
+{
+    squares[i].onclick = function() 
+    {
+        if(this.innerHTML !== 'X' && this.innerHTML !== 'O')
+        {
+            if (player === 'X') 
+            {
+                this.innerHTML = 'X';
+                player = 'O';
+                displayturn();
+            } 
+            else 
+            {
+                this.innerHTML = 'O';
+                player = 'X';
+                displayturn();
+            }
+            winner();
+            displaydraw();
+        }
+    }
+}
+
+// Function to check the winner
+function winner() 
+{
+    if(squares[0].innerHTML !== '' && squares[0].innerHTML === squares[1].innerHTML && squares[0].innerHTML === squares[2].innerHTML) 
+    {
+        displaywinder(squares[0].innerHTML);
+        reset();
+    } 
+    else if(squares[3].innerHTML !== '' && squares[3].innerHTML === squares[4].innerHTML && squares[3].innerHTML === squares[5].innerHTML) 
+    {
+        displaywinder(squares[3].innerHTML);
+        reset();
+    } 
+    else if(squares[6].innerHTML !== '' && squares[6].innerHTML === squares[7].innerHTML && squares[6].innerHTML === squares[8].innerHTML) 
+    {
+        displaywinder(squares[6].innerHTML)
+        reset();
+    } 
+    else if(squares[0].innerHTML !== '' && squares[0].innerHTML === squares[3].innerHTML && squares[0].innerHTML === squares[6].innerHTML) 
+    {
+        displaywinder(squares[0].innerHTML);
+        reset();
+    } 
+    else if(squares[1].innerHTML !== '' && squares[1].innerHTML === squares[4].innerHTML && squares[1].innerHTML === squares[7].innerHTML) 
+    {
+        displaywinder(squares[1].innerHTML);
+        reset();
+    } 
+    else if(squares[2].innerHTML !== '' && squares[2].innerHTML === squares[5].innerHTML && squares[2].innerHTML === squares[8].innerHTML) 
+    {
+        displaywinder(squares[2].innerHTML);
+        reset();
+    } 
+    else if(squares[0].innerHTML !== '' && squares[0].innerHTML === squares[4].innerHTML && squares[0].innerHTML === squares[8].innerHTML) 
+    {
+        displaywinder(squares[0].innerHTML);
+        reset();
+    } 
+    else if(squares[2].innerHTML !== '' && squares[2].innerHTML === squares[4].innerHTML && squares[2].innerHTML === squares[6].innerHTML) 
+    {
+        displaywinder(squares[2].innerHTML);
+        reset();
+    }
+}
+
+// Function to reset the game
+function reset() 
+{
+    for(i = 0; i < squares.length; i++) 
+    {
+        squares[i].innerHTML = '';
+    }
+    player = 'X';
+    turn.innerHTML = '';
+}
+
+// Button to reset the game
+resetBtn.onclick = function() 
+{
+    reset();
+}
+
+// Function to display the winner
+function displaywinder(winner)
+{
+    winnerplayer.innerHTML = 'Player ' + winner + ' wins!';
+}
+
+// Function to display who's turn it is
+function displayturn()
+{
+    turn.innerHTML = 'Player ' + player + ' turn';
+}
+
+// Function to check if there is a draw
+function isDraw() 
+{
+    for (var i = 0; i < squares.length; i++) 
+    {
+      if (squares[i].innerHTML === '') 
+      {
+        return false;
+      }
+    }
+    return true;
+}
+// Function to display draw
+function displaydraw()
+{
+    if(isDraw() === true)
+    {
+        winnerplayer.innerHTML = 'Draw!';
+    }
+}
